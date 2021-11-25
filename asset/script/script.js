a355acaf97cc9a6b405fc6648fe2cdc9d4be2de5
João Pedro de Almeida <78094903+JoaoPedroAC@users.noreply.github.com>
2021-11-25 18:03:53 +0000

    add function printInDoc() e organizei o código

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,15 +1,23 @@
 /* dia da semana, a data dd/mm/aaaa ás 00:00 -> objetivo*/
 // Essa função cria uma div filha e tag p, onde será escrito tudo oq precisamos
-function createDiv(obj) {
+function printInDoc(p, obj){
+  p.innerHTML= obj;
+}
+printInDoc(createP(createDiv()),`${convertDay()}, ${buildDate()} ás`);
+
+function createDiv() {
 	const momDiv = document.getElementById('main');
 	const childDiv = document.createElement('div');
 	childDiv.setAttribute('class', 'children');
-	momDiv.appendChild(childDiv);
-	const p = document.createElement('p');
-	childDiv.appendChild(p);
-	return (p.innerHTML = obj);
+  momDiv.appendChild(childDiv);
+	return childDiv;
 }
-createDiv(`${convertDay()}, ${buildDate()} ás`);
+
+function createP(div){
+  const p = document.createElement('p');
+	div.appendChild(p);
+	return p;
+}
 
 //  vai apresentar o nome do dia
 function convertDay() {
@@ -26,9 +34,7 @@ function convertDay() {
 	];
 	return days[atualDate];
 }
-function createP(){
-  
-}
+
 function convertMonth(){
 	const date = new Date();
 
