46120059677b9688bd2028b48a151bb622b3619c
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-11-25 22:45:27 -0300

    consertei e agr ta tudo dando certo

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,32 +1,27 @@
 /* dia da semana, a data dd/mm/aaaa ás 00:00 -> objetivo*/
-
-// essa function executa no html oq eu precisar
-function printInDoc(p, obj){
-  p.innerHTML= obj;
-}
-printInDoc(createP(),`${convertDay()}, ${buildDate()} ás`);
-
+let relogio = watch();
 // Essa função cria uma div filha;
 function createDiv() {
 	const momDiv = document.getElementById('main');
 	const childDiv = document.createElement('div');
 	childDiv.setAttribute('class', 'children');
-  momDiv.appendChild(childDiv);
+	momDiv.appendChild(childDiv);
 	return childDiv;
 }
 
 // Essa função cria uma tag P que está inserida na DIV filha;
-function createP(){
-  const div = createDiv();
-  const p = document.createElement('p');
+function createP(obj) {
+	const div = createDiv();
+	const p = document.createElement('p');
+	p.classList.add('relogio')
 	div.appendChild(p);
-	return p;
+	p.innerHTML = obj;
 }
-
+createP(`${convertDay()}, ${buildDate()} ás ${relogio}`)
 //  vai apresentar o nome dos dias;
 function convertDay() {
 	const date = new Date();
-	let atualDate = date.getDay(); 
+	const atualDate = date.getDay();
 	const days = [
 		'Domingo',
 		'Segunda-feira',
@@ -34,16 +29,53 @@ function convertDay() {
 		'Quarta-feira',
 		'Quinta-feira',
 		'Sexta-feira',
-		'Sábado'
+		'Sábado',
 	];
 	return days[atualDate];
 }
 
-function convertMonth(){
+function convertMonth() {
 	const date = new Date();
-
+	month = [
+		'Janeiro',
+		'Fvereiro',
+		'Março',
+		'Abril',
+		'Maio',
+		'Junho',
+		'Julho',
+		'Agosto',
+		'Setembro',
+		'Outubro',
+		'Novembro',
+		'Dezembro',
+	];
+	const atualMonth = date.getMonth();
+	return month[atualMonth];
 }
-function buildDate(){
+// vai aplicar zero a esquerda quando os numeros forem menor q 10
+function zeroEsquerda(number) {
+	let zero = number < 10 ? `0${number}` : number;
+	return zero;
+}
+//  Essa função retorna a data do mesmo dia do acesso
+function buildDate() {
 	const date = new Date();
-    
+	const day = zeroEsquerda(date.getDate());
+	const month = convertMonth();
+	const year = date.getFullYear();
+	return `${day} / ${month} / ${year}`;
 }
+function watch() {
+	const time = new Date();
+	const hours = zeroEsquerda(time.getHours());
+	const minutes = zeroEsquerda(time.getMinutes());
+	const seconds = zeroEsquerda(time.getSeconds());
+	let relogio = `${hours} : ${minutes} : ${seconds}`;
+	return relogio;
+}
+setInterval(function () {
+	relogio = watch();
+	const pClass = document.querySelector('.relogio');
+	pClass.innerHTML = `${convertDay()}, ${buildDate()} ás ${relogio}`;
+}, 1000);
